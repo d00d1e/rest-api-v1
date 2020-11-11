301deadeafd23c1f9d6f3b46901f992533a843f7
d00d1e <lee.julianay@gmail.com>
2020-11-11 14:54:18 -0800

    first commit

---
+++ routes/subscribers.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const subscriber = require('../models/subscriber');
+const router = express.Router();
+const Subscriber = require('../models/subscriber');
+
+// GET all 
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET one
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
+
+// CREATE one
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  });
+
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+});
+
+
+// UPDATE one
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+})
+
+// DELETE one
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: 'Subscriber deleted '})
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
+});
+
+// middleware
+async function getSubscriber(req, res, next) {
+  let subscriber; 
+
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  // can call for all other /:id routes
+  res.subscriber = subscriber;
+  next();
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+// from .env
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.error('Connected to Database.'));
+
+// allow server to accept json via middleware
+app.use(express.json());
+
+//ROUTES
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
+app.listen(5000, () => console.log('Server started...'));
